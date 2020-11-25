5c2741a639996a99996d86d40fa9abb4c4f1aaa3
tar <chengye1@xiaomi.com>
2020-11-25 20:41:30 +0800

    works

---
+++ vendor/composer/installed.php
@@ -0,0 +1,249 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'yiisoft/yii2-app-basic',
+  ),
+  'versions' => 
+  array (
+    'bower-asset/bootstrap' => 
+    array (
+      'pretty_version' => 'v3.4.1',
+      'version' => '3.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '68b0d231a13201eb14acd3dc84e51543d16e5f7e',
+    ),
+    'bower-asset/inputmask' => 
+    array (
+      'pretty_version' => '3.3.11',
+      'version' => '3.3.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e670ad62f50c738388d4dcec78d2888505ad77b',
+    ),
+    'bower-asset/jquery' => 
+    array (
+      'pretty_version' => '3.5.1',
+      'version' => '3.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4c0e4becb8263bb5b3e6dadc448d8e7305ef8215',
+    ),
+    'bower-asset/punycode' => 
+    array (
+      'pretty_version' => 'v1.3.2',
+      'version' => '1.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38c8d3131a82567bfef18da09f7f4db68c84f8a3',
+    ),
+    'bower-asset/yii2-pjax' => 
+    array (
+      'pretty_version' => '2.0.7.1',
+      'version' => '2.0.7.1',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aef7b953107264f00234902a3880eb50dafc48be',
+    ),
+    'cebe/markdown' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9bac5e971dd391e2802dca5400bbeacbaea9eb86',
+    ),
+    'doctrine/lexer' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e864bbf5904cb8f5bb334f99209b48018522f042',
+    ),
+    'egulias/email-validator' => 
+    array (
+      'pretty_version' => '2.1.23',
+      'version' => '2.1.23.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa792ad1853ae2bc60528dd3e5cbf4542d3c1df',
+    ),
+    'ezyang/htmlpurifier' => 
+    array (
+      'pretty_version' => 'v4.13.0',
+      'version' => '4.13.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '08e27c97e4c6ed02f37c5b2b20488046c8d90d75',
+    ),
+    'fzaninotto/faker' => 
+    array (
+      'pretty_version' => 'v1.9.1',
+      'version' => '1.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc10d778e4b84d5bd315dad194661e091d307c6f',
+    ),
+    'opis/closure' => 
+    array (
+      'pretty_version' => '3.6.1',
+      'version' => '3.6.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '943b5d70cc5ae7483f6aff6ff43d7e34592ca0f5',
+    ),
+    'phpspec/php-diff' => 
+    array (
+      'pretty_version' => 'v1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fc1156187f9f6c8395886fe85ed88a0a245d72e9',
+    ),
+    'swiftmailer/swiftmailer' => 
+    array (
+      'pretty_version' => 'v6.2.3',
+      'version' => '6.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '149cfdf118b169f7840bbe3ef0d4bc795d1780c9',
+    ),
+    'symfony/polyfill-iconv' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c536646fdb4f29104dd26effc2fdcb9a5b085024',
+    ),
+    'symfony/polyfill-intl-idn' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3b75acd829741c768bc8b1f84eb33265e7cc5117',
+    ),
+    'symfony/polyfill-intl-normalizer' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '727d1096295d807c309fb01a851577302394c897',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39d483bdf39be819deabf04ec872eb0b2410b531',
+    ),
+    'symfony/polyfill-php72' => 
+    array (
+      'pretty_version' => 'v1.20.0',
+      'version' => '1.20.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cede45fcdfabdd6043b3592e83678e42ec69e930',
+    ),
+    'yiisoft/yii2' => 
+    array (
+      'pretty_version' => '2.0.38',
+      'version' => '2.0.38.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fd01e747cc66a049ec105048f0ab8dfbdf60bf4b',
+    ),
+    'yiisoft/yii2-app-basic' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'yiisoft/yii2-bootstrap' => 
+    array (
+      'pretty_version' => '2.0.10',
+      'version' => '2.0.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '073c9ab0a4eb71f2485d84c96a1967130300d8fc',
+    ),
+    'yiisoft/yii2-composer' => 
+    array (
+      'pretty_version' => '2.0.10',
+      'version' => '2.0.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '94bb3f66e779e2774f8776d6e1bdeab402940510',
+    ),
+    'yiisoft/yii2-debug' => 
+    array (
+      'pretty_version' => '2.1.13',
+      'version' => '2.1.13.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '696712a2a3565b1a072ad3c9d298e262967e8282',
+    ),
+    'yiisoft/yii2-faker' => 
+    array (
+      'pretty_version' => '2.0.4',
+      'version' => '2.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3df62b1dcb272a8413f9c6e532c9d73f325ccde1',
+    ),
+    'yiisoft/yii2-gii' => 
+    array (
+      'pretty_version' => '2.1.4',
+      'version' => '2.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd879cb186361fbc6f71a2d994d580b5a071a5642',
+    ),
+    'yiisoft/yii2-swiftmailer' => 
+    array (
+      'pretty_version' => '2.1.2',
+      'version' => '2.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '09659a55959f9e64b8178d842b64a9ffae42b994',
+    ),
+  ),
+);
